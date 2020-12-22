9c0f03233acc2124db5e866432fe2d0e8450d764
yashkumar3000 <yashkumar20009@gmail.com>
2020-12-22 10:12:40 +0530

    added request to api

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,11 @@ form.onsubmit = async(event) => {
     const json = await response.json();
 
     // fetch json and data and change data in the respective span in html using dom
-
+    document.getElementById('temp').innerHTML=json.temp;
+    document.getElementById('wind').innerHTML=json.windSpeed;
+    document.getElementById('pres').innerHTML=json.pressure;
+    document.getElementById('humid').innerHTML=json.humid;
+    // document.getElementById('image').src=json.iconurl;
+    document.getElementById('place').innerHTML=json.city +" "+json.country + "<img class='weather-cond' src=' "+json.iconurl+"'>";
     console.log(json);
 };
--- server.js
+++ server.js
@@ -1,25 +1,44 @@
 const express = require("express");
 const path = require("path");
-const request = require("request");
+//const request = require("request");
+const https = require("https") ; 
 
 const app = express();
-
-const port = 3000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
 app.get("/weather/:city",(req,res) => {
-    const city = req.params.city;
+    var city = req.params.city;
+    const appid = "33d92e56156fee541dac0c7b78ad2285" ;
+    url  =  "https://api.openweathermap.org/data/2.5/weather?q="+city +"&appid="+appid ;
     // fetch weather from openweather
-    
-    //send weather data to frontend
+    https.get(url, (response) =>{
 
-    // send icon uri 
-    // send temperature
-    // send wind speed
-    // send pressure
+         response.on("data"  , (data)=>{
+             const weatherData = JSON.parse(data) ;
+             console.log(weatherData);
+
+             // send icon uri/url
+             
+             const weatherObject = {
+                iconurl:"http://openweathermap.org/img/w/" +weatherData.weather[0].icon + ".png",
+                temp: Number(weatherData.main.temp)-273,
+                windSpeed:weatherData.wind.speed,
+                pressure:weatherData.main.pressure,
+                humid:weatherData.main.humidity,
+                country:weatherData.sys.country,
+                city:weatherData.name
+             };
+              
+             res.send(weatherObject);
+         } )
+         
+    } )
+   
 });
 
+
+const port = 3000 || process.env.PORT;
 app.listen(port,console.log("Server running on port " + port));
